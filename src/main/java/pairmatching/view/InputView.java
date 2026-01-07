9a4cc5096e8aa38ac0168779212955fb689dbbb0
khcho96 <khcho1492@gmail.com>
2026-01-07 10:14:42 +0900

    test: 테스트 코드 추가

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -32,7 +32,7 @@ public class InputView {
     public static String readRematch() {
         System.out.println("\n"
                 + "매칭 정보가 있습니다. 다시 매칭하시겠습니까?\n"
-                + "네 | 아니오\n");
+                + "네 | 아니오");
         return Console.readLine();
     }
 }
--- src/test/java/pairmatching/ApplicationTest.java
+++ src/test/java/pairmatching/ApplicationTest.java
@@ -7,7 +7,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.Arrays;
 import org.junit.jupiter.api.Test;
-import pairmatching.Application;
 
 class ApplicationTest extends NsTest {
 
@@ -31,6 +30,17 @@ class ApplicationTest extends NsTest {
                 runException("1", "백엔드, 레벨1, 오징어게임");
                 assertThat(output()).contains(ERROR_MESSAGE);
             }
+        );
+    }
+
+    @Test
+    void 홀수_인원_페어_매칭() {
+        assertShuffleTest(
+                () -> {
+                    run("1", "백엔드, 레벨1, 자동차경주", "Q");
+                    assertThat(output()).contains("태웅 : 백호", "치수 : 태섭 : 소연");
+                },
+                Arrays.asList("태웅", "백호", "치수", "태섭", "소연")
         );
     }
 
