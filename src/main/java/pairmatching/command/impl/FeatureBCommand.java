ac641354b9a0187b36afc71e92ee4b6f19e3b7c9
khcho96 <khcho1492@gmail.com>
2026-01-07 10:09:21 +0900

    feat: 페어 조회 기능 구현

--- src/main/java/pairmatching/command/impl/FeatureBCommand.java
+++ src/main/java/pairmatching/command/impl/FeatureBCommand.java
@@ -1,7 +1,13 @@
 package pairmatching.command.impl;
 
 import pairmatching.command.Command;
+import pairmatching.constant.Content;
+import pairmatching.constant.ErrorMessage;
 import pairmatching.service.MatchingService;
+import pairmatching.util.InputParser;
+import pairmatching.util.Retry;
+import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
 
 public class FeatureBCommand implements Command {
 
@@ -13,6 +19,25 @@ public class FeatureBCommand implements Command {
 
     @Override
     public void execute() {
+        Content content;
+        while (true) {
+            content = getContent();
 
+            if (service.MatchingExists(content)) {
+                break;
+            }
+
+            OutputView.printErrorMessage(new IllegalArgumentException(ErrorMessage.NO_EXIST_MATCHING_ERROR.getErrorMessage()));
+        }
+
+        OutputView.printMatching(service.getMatching(content));
+    }
+
+    private static Content getContent() {
+        Content content;
+        content = Retry.retryUntilSuccess(() ->
+                InputParser.parseContent(InputView.readContent())
+        );
+        return content;
     }
 }
